2a5f984d1777ea392c8c2fb1f06ce692f8dc9a26
arshad24B11CS284 <mohammedarshad13102006@gmail.com>
2025-05-01 16:07:22 +0530

    Time: 0 ms (100%), Space: 7.8 MB (30.45%) - LeetHub

---
+++ 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.c
@@ -0,0 +1,15 @@
+
+int numberOfMatches(int n){
+     int m =0;
+     while(n>1){
+        if(n%2==0){
+            m = m + n/2;
+            n = n/2;
+        } else {
+            m = m + (n-1)/2;
+            n = (n-1)/2+1;
+        }
+
+     }
+     return m;
+}
